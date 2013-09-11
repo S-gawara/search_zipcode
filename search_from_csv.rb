05f8f7a60af844bd29ed5ce3bcde1ba547030a73
dangerousanimal <hideaki_ishii1204@yahoo.co.jp>
2013-09-12 00:09:39 +0900

    create search zipcode app

---
+++ search_from_csv.rb
@@ -0,0 +1,15 @@
+code = ARGV[0]
+
+# 処理速度を計測する
+start_time = Time.now
+
+File.open("KEN_ALL.CSV", "r:shift_jis").each_line do |line|
+	line.chomp! #末尾が改行の場合、その改行文字を削る
+	rows = line.split(/,/)
+	zipcode = rows[2].gsub(/"/, '')
+	if zipcode == code
+		puts line.encode("utf-8")
+	end
+end
+
+p Time.now - start_time

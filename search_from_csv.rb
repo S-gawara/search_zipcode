1eca236761c0ede53a9968bbc88cc7080ddd266a
Hideaki ishii <hideaki_ishii1204@yahoo.co.jp>
2014-02-16 10:30:11 +0900

    fix layout

--- search_from_csv.rb
+++ search_from_csv.rb
@@ -4,12 +4,12 @@ code = ARGV[0]
 start_time = Time.now
 
 File.open("KEN_ALL.CSV", "r:shift_jis").each_line do |line|
-	line.chomp! #末尾が改行の場合、その改行文字を削る
-	rows = line.split(/,/)
-	zipcode = rows[2].gsub(/"/, '')
-	if zipcode == code
-		puts line.encode("utf-8")
-	end
+  line.chomp! #末尾が改行の場合、その改行文字を削る
+  rows = line.split(/,/)
+  zipcode = rows[2].gsub(/"/, '')
+  if zipcode == code
+    puts line.encode("utf-8")
+  end
 end
 
 p Time.now - start_time
